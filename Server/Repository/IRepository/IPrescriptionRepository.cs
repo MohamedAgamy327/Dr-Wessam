7ae855484556be9ff4a3652664bb1cb9e7cbc2e9
alaa saleh <alaa.saleh@vodafone.come>
2020-01-04 15:28:22 +0200

    presCroptionRepos

---
+++ Server/Repository/IRepository/IPrescriptionRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Repository.IRepository
+{
+    public interface IPrescriptionRepository
+    {
+        Task<Prescription> Add(Prescription prescription);
+        Prescription Edit(Prescription prescription);
+        void Remove(Prescription prescription);
+        Task<Prescription> Get(int id);
+        Task<IEnumerable<Prescription>> Get();
+    }
+}
---
+++ Server/Repository/Repository/PrescriptionRepository.cs
@@ -0,0 +1,45 @@
+﻿using Data.Context;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Repository.IRepository;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Repository.Repository
+{
+    public class PrescriptionRepository : IPrescriptionRepository
+    {
+        private readonly ApplicationContext context;
+        public PrescriptionRepository(ApplicationContext context)
+        {
+            this.context = context;
+        }
+        public async Task<Prescription> Add(Prescription patient)
+        {
+            await context.Prescriptions.AddAsync(patient);
+            return patient;
+        }
+        public Prescription Edit(Prescription prescription)
+        {
+            context.Entry(prescription).State = EntityState.Modified;
+            return prescription;
+        }
+        public async Task<Prescription> Get(int id)
+        {
+            return await context.Prescriptions.FindAsync(id);
+        }
+        public async Task<IEnumerable<Prescription>> Get()
+        {
+            return await context.Prescriptions.Include(i => i.PatientId).ToListAsync();
+        }
+
+        public void Remove(Prescription prescription)
+        {
+            context.Remove(prescription);
+        }
+
+
+
+        
+    }
+}
